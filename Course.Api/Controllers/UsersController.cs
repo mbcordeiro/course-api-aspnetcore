e39baa0350fc98414105a3452cfa84118ce04da6
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-06-19 01:16:31 -0300

    Login controller and view model input

---
+++ Course.Api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Course.Api.Models.Users;
+
+namespace Course.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult LogIn(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
